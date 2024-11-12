15b53365b8e6ce0bd4271b0acdfa0af58cd4a1ab
Jasper Rijkeboer <j.rijkeboer@4dotnet.nl>
2024-11-12 15:39:47 +0100

    update nuget

--- DiscordBot/Configuration/DiscordBotSettings.cs
+++ DiscordBot/Configuration/DiscordBotSettings.cs
@@ -1,3 +1,5 @@
+namespace DiscordBot.Configuration;
+
 public class DiscordBotSettings
 {
     public string? BotToken { get; init; }
--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -1,44 +1,56 @@
 ﻿using Discord;
 using Discord.WebSocket;
+
 using Microsoft.Extensions.Configuration;
 
-internal class Program {
-    private static async Task Main (string[] args) {
-        Console.WriteLine ("Hello, World!");
-        var environment = Environment.GetEnvironmentVariable ("ASPNETCORE_ENVIRONMENT");
+namespace DiscordBot;
 
-        var configuration = new ConfigurationBuilder ()
-            .SetBasePath (Directory.GetCurrentDirectory ())
-            .AddJsonFile ("appsettings.json", optional : false, reloadOnChange : true)
-            .AddJsonFile ($"appsettings.{environment}.json", true, true)
-            .AddEnvironmentVariables ()
-            .Build ();
+internal static class Program
+{
+    private static async Task Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 
-        string botToken = configuration.GetSection ("DiscordBotSettings") ["BotToken"] ?? "";
-        Console.WriteLine ($"Using botToken: '{botToken}'");
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{environment}.json", true, true)
+            .AddEnvironmentVariables()
+            .Build();
 
-        var client = new DiscordSocketClient ();
-        await client.LoginAsync (TokenType.Bot, botToken);
-        await client.StartAsync ();
+        string botToken = configuration.GetSection("DiscordBotSettings")["BotToken"] ?? "";
+        Console.WriteLine($"Using botToken: '{botToken}'");
 
-        client.UserUpdated += async (oldUser, newUser) => {
+        var client = new DiscordSocketClient();
+        await client.LoginAsync(TokenType.Bot, botToken);
+        await client.StartAsync();
+
+        client.UserUpdated += async (oldUser,
+            newUser) =>
+        {
             // Create a Channel object by searching for a channel named '#logs' on the server the ban occurred in.
-            var groupChannels = await client.GetGroupChannelsAsync ();
-            var logChannel = groupChannels.Where (x => x.Name == "logs").FirstOrDefault ();
+            var groupChannels = await client.GetGroupChannelsAsync();
+            var logChannel = groupChannels.FirstOrDefault(x => x.Name == "logs");
 
             if (
-                oldUser.Activities?.FirstOrDefault ()?.Name != newUser.Activities?.FirstOrDefault ()?.Name &&
-                newUser.Activities?.FirstOrDefault ()?.Type == ActivityType.Playing) {
-                var gameName = newUser.Activities.FirstOrDefault ()?.Name;
-                var usersPlaying = client.Guilds.SelectMany (g => g.Users)
-                    .Where (u => u.Activities?.FirstOrDefault ()?.Name == gameName)
-                    .Select (u => u.Username);
+                oldUser.Activities?.FirstOrDefault()?.Name != newUser.Activities?.FirstOrDefault()?.Name &&
+                newUser.Activities?.FirstOrDefault()?.Type == ActivityType.Playing)
+            {
+                var gameName = newUser.Activities.FirstOrDefault()?.Name;
+                var usersPlaying = client.Guilds.SelectMany(g => g.Users)
+                    .Where(u => u.Activities?.FirstOrDefault()?.Name == gameName)
+                    .Select(u => u.Username);
 
-                Console.WriteLine ($"{string.Join(", ", usersPlaying)} are playing {gameName}");
+                if (logChannel != null)
+                {
+                    await logChannel.SendMessageAsync($"User {newUser.Username} changed activity ({newUser.Activities?.FirstOrDefault()?.Name})");
+                }
+                Console.WriteLine($"{string.Join(", ", usersPlaying)} are playing {gameName}");
             }
         };
 
-        await Task.Delay (-1);
+        await Task.Delay(-1);
     }
 
 }
