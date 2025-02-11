9653e1a186cbdf0577008fed42e48a74caefdae8
Jasper Rijkeboer (4DotNet) <j.rijkeboer@4dotnet.nl>
2025-02-11 13:47:55 +0100

    Add Scrutor package to scan for Discord modules

--- DiscordBot.Service/Extensions/ServiceCollectionExtensions.cs
+++ DiscordBot.Service/Extensions/ServiceCollectionExtensions.cs
@@ -8,33 +8,44 @@ public static class ServiceCollectionExtensions
 {
     public static void AddApplicationServices(this IHostApplicationBuilder builder)
     {
-        builder.AddNpgsqlDbContext<DiscordBotDbContext>("discordbotdb", configureDbContextOptions: dbContextOptionsBuilder =>
-        {
-            dbContextOptionsBuilder.UseNpgsql(builder =>
+        builder.AddNpgsqlDbContext<DiscordBotDbContext>(
+            "discordbotdb",
+            configureDbContextOptions: dbContextOptionsBuilder =>
             {
-                builder.EnableRetryOnFailure();
-            });
-        });
-        
-        builder.Services.AddOptions<DiscordBotOptions>()
+                dbContextOptionsBuilder.UseNpgsql(builder =>
+                {
+                    builder.EnableRetryOnFailure();
+                });
+            }
+        );
+
+        builder
+            .Services.AddOptions<DiscordBotOptions>()
             .BindConfiguration(nameof(DiscordBotOptions));
-        
+
         builder.Services.AddHostedService<DiscordBotService>();
         builder.Services.AddSingleton<CancellationTokenSource>();
+
         builder.AddDiscordServices();
     }
 
     private static void AddDiscordServices(this IHostApplicationBuilder builder)
     {
-        builder.Services.AddSingleton(new DiscordSocketConfig()
-        {
-            AlwaysDownloadUsers = true,
-            LogLevel = Discord.LogSeverity.Verbose,
-            MessageCacheSize = 1000,
-            GatewayIntents = GatewayIntents.All
-        });
+        builder.Services.AddSingleton(
+            new DiscordSocketConfig()
+            {
+                AlwaysDownloadUsers = true,
+                LogLevel = Discord.LogSeverity.Verbose,
+                MessageCacheSize = 1000,
+                GatewayIntents = GatewayIntents.All,
+            }
+        );
 
         builder.Services.AddSingleton<DiscordSocketClient>();
         builder.Services.AddSingleton<CommandService>();
+
+        builder.Services.Scan(scan =>
+            scan.FromAssembliesOf(typeof(ModuleBase)).AddClasses().AsImplementedInterfaces()
+        );
     }
 }
